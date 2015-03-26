92d5665465097f2fde550517666a6cab3a7691cb
Rasmus Kuschel <rasmus.kuschel@immobilienscout24.de>
2015-03-26 10:46:40 +0100

    snap implemented - correct, but slow

--- src/snapper/Snapper.scala
+++ src/snapper/Snapper.scala
@@ -22,12 +22,26 @@ case class Lamp(input: Powerable) extends Powerable {
 
 object Snapper extends App {
 
-  println("Hello world")
+//  val N = 4
+//  val K = 47
+//
+//  val lampOn = snap(K, buildChain(N)).isPoweredOn
+//  println(lampOn)
 
-  val N = 4
-  println(buildChain(N))
+  import scala.io.Source
+  val lines = Source.fromFile("src/snapper/A-large-practice.in").getLines.zipWithIndex
+  lines.next // drop #cases
 
-  val x = Lamp(SnapperOff(SnapperOff(Plug)))
+  while (lines.hasNext) {
+    val (line, i) = lines.next
+
+    val N = line.split(" ")(0).toInt
+    val K = line.split(" ")(1).toInt
+
+    val lampOn = snapK(K, buildChain(N)).isPoweredOn
+
+    println("Case #" + i + ": " + (if (lampOn) "ON" else "OFF"))
+  }
 
   def buildChain(n: Integer): Powerable = {
     def buildSnapperChain(n: Integer): Powerable =
@@ -36,7 +50,19 @@ object Snapper extends App {
     Lamp(buildSnapperChain(n))
   }
 
-  def snap(chain: Powerable): Powerable = ???
+  def snap(chain: Powerable): Powerable = chain match {
+    case Lamp(inner) => Lamp(snap(inner))
 
+    case SnapperOn(inner) if inner.isPoweredOn => SnapperOff(snap(inner))
+    case SnapperOn(inner) => SnapperOn(snap(inner))
+
+    case SnapperOff(inner) if inner.isPoweredOn => SnapperOn(snap(inner))
+    case SnapperOff(inner) => SnapperOff(snap(inner))
+
+    case Plug => Plug
+  }
+
+  def snapK(k: Integer, chain: Powerable): Powerable =
+    if (k==0) chain else snapK(k-1, snap(chain))
 }
 
